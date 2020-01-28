a104c5fee65a90d5f3db7139a17bd2f1faa0f22a
JCPasillas <carlospasillas562@gmail.com>
2020-01-28 12:18:09 -0800

    adding a comment

--- app/src/main/java/com/example/servermatch/cecs445/ui/addcustomer/AddCustomerFragment.java
+++ app/src/main/java/com/example/servermatch/cecs445/ui/addcustomer/AddCustomerFragment.java
@@ -14,6 +14,7 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.servermatch.cecs445.R;
 
+//test
 public class AddCustomerFragment extends Fragment {
 
     private AddCustomerViewModel addCustomerViewModel;
