91ab222c74d80cb7f39b7a3941acead9474427fd
Andrew-Delgado <andrewdelgado017@gmail.com>
2020-01-29 18:40:13 -0800

    splitting the menu fragment with R.V. and bottom text

--- app/src/main/java/com/example/servermatch/cecs445/ui/menu/MenuFragment.java
+++ app/src/main/java/com/example/servermatch/cecs445/ui/menu/MenuFragment.java
@@ -1,5 +1,6 @@
 package com.example.servermatch.cecs445.ui.menu;
 
+import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,8 +12,11 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.servermatch.cecs445.R;
+import com.example.servermatch.cecs445.Utils.ListAdapter;
 
 public class MenuFragment extends Fragment {
 
@@ -20,16 +24,28 @@ public class MenuFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        menuViewModel =
-                ViewModelProviders.of(this).get(MenuViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_menu, container, false);
-        final TextView textView = root.findViewById(R.id.text_menu);
-        menuViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
+
+        View view = inflater.inflate(R.layout.fragment_menu,container,false);
+
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recycler_view_menu);
+
+        ListAdapter listAdapter = new ListAdapter();
+        recyclerView.setAdapter(listAdapter);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+
+        return view;
     }
 }
+
+//menuViewModel =
+//        ViewModelProviders.of(this).get(MenuViewModel.class);
+//        View root = inflater.inflate(R.layout.fragment_menu, container, false);
+//final TextView textView = root.findViewById(R.id.text_menu);
+//        menuViewModel.getText().observe(this, new Observer<String>() {
+//@Override
+//public void onChanged(@Nullable String s) {
+//        textView.setText(s);
+//        }
+//        });
+//        return root;
---
+++ app/src/main/java/com/example/servermatch/cecs445/ui/menu/TestData.java
@@ -0,0 +1,18 @@
+package com.example.servermatch.cecs445.ui.menu;
+
+import com.example.servermatch.cecs445.R;
+
+public class TestData {
+
+    public static String[] title = new String[] {
+            "Andrew",
+            "Delgado",
+            "CECS-445"
+    };
+
+    public static int[] picturePath = new int[]{
+            R.drawable.ic_menu_camera,
+            R.drawable.ic_menu_gallery,
+            R.drawable.ic_menu_send
+    };
+}
