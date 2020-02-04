e38995d7a402919afb120586544e090985fc8646
brandonnhem <brandonnhem@gmail.com>
2020-02-04 11:40:44 -0800

    Chang add menu item

--- app/src/main/java/com/example/servermatch/cecs445/ui/addmenuitem/AddMenuItemFragment.java
+++ app/src/main/java/com/example/servermatch/cecs445/ui/addmenuitem/AddMenuItemFragment.java
@@ -8,28 +8,20 @@ import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.servermatch.cecs445.R;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class AddMenuItemFragment extends Fragment {
 
-    private AddMenuItemViewModel addMenuItemViewModel;
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        addMenuItemViewModel =
-                ViewModelProviders.of(this).get(AddMenuItemViewModel.class);
         View root = inflater.inflate(R.layout.fragment_add_menu_item, container, false);
-        final TextView textView = root.findViewById(R.id.text_add_menu_item);
-        addMenuItemViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
         return root;
     }
 }
