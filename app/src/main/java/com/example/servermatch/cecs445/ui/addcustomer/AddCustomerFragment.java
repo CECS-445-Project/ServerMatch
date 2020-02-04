f66f67d2f5bed7444d5eb18b51aceb7d4e66c693
JCPasillas <carlospasillas562@gmail.com>
2020-02-04 13:15:36 -0800

    Updated fragment_add_customer.xml

--- app/src/main/java/com/example/servermatch/cecs445/ui/addcustomer/AddCustomerFragment.java
+++ app/src/main/java/com/example/servermatch/cecs445/ui/addcustomer/AddCustomerFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
@@ -13,24 +14,29 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.servermatch.cecs445.R;
+import com.google.android.material.textfield.TextInputLayout;
 
-//test
 public class AddCustomerFragment extends Fragment {
 
     private AddCustomerViewModel addCustomerViewModel;
+    private TextInputLayout addCustomerFname;
+    private TextInputLayout addCustomerLname;
+    private TextInputLayout addCustomerEmail;
+    private TextInputLayout addCustomerPhone;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         addCustomerViewModel =
                 ViewModelProviders.of(this).get(AddCustomerViewModel.class);
         View root = inflater.inflate(R.layout.fragment_add_customer, container, false);
-        final TextView textView = root.findViewById(R.id.text_add_customer);
-        addCustomerViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        addCustomerFname = root.findViewById(R.id.add_customer_fname);
+        addCustomerLname = root.findViewById(R.id.add_customer_lname);
+        addCustomerEmail = root.findViewById(R.id.add_customer_email);
+        addCustomerPhone = root.findViewById(R.id.add_customer_phone);
+
         return root;
     }
+
+
 }
