82ea60b92b4d5b8dea19fcdc18eec9e3ecac572a
shadow88s <howardshowered@gmail.com>
2020-02-17 14:29:47 -0800

    -fixed loading firestore data into MenuItemsList

--- app/src/main/java/com/example/servermatch/cecs445/repositories/MenuItemRepo.java
+++ app/src/main/java/com/example/servermatch/cecs445/repositories/MenuItemRepo.java
@@ -6,11 +6,8 @@ package com.example.servermatch.cecs445.repositories;
 import android.content.Context;
 import android.util.Log;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.lifecycle.MutableLiveData;
-
-import com.example.servermatch.cecs445.R;
 import com.example.servermatch.cecs445.models.MenuItem;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -48,19 +45,20 @@ public class MenuItemRepo {
     }
 
     private void loadMenuItems(){
+
         db.collection("MenuItem").get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
             @Override
             public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
 
-                if(!queryDocumentSnapshots.isEmpty()){
+                if (!queryDocumentSnapshots.isEmpty()) {
                     List<DocumentSnapshot> list = queryDocumentSnapshots.getDocuments();
 
-                    for(DocumentSnapshot documentSnapshot : list){
+                    for (DocumentSnapshot documentSnapshot : list) {
                         dataSet.add(documentSnapshot.toObject(MenuItem.class));
                     }
                 }
 
-                Log.e(TAG, "onSuccess: added" );
+                Log.e(TAG, "onSuccess: added");
 
             }
         }).addOnFailureListener(new OnFailureListener() {
@@ -69,6 +67,7 @@ public class MenuItemRepo {
                 Log.e(TAG, "onFailure: " + e.toString());
             }
         });
+
     }
 
     public void addMenuItem(MenuItem newItem, final Context context){
