291e6f9e0ed8f0a951942547a961efd5f812966c
brandonnhem <brandonnhem@gmail.com>
2020-02-20 00:13:08 -0800

    Can now open from gallery and upload images to Firebase

--- app/src/main/java/com/example/servermatch/cecs445/ui/addmenuitem/AddMenuItemFragment.java
+++ app/src/main/java/com/example/servermatch/cecs445/ui/addmenuitem/AddMenuItemFragment.java
@@ -1,30 +1,42 @@
 package com.example.servermatch.cecs445.ui.addmenuitem;
 
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.MimeTypeMap;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.example.servermatch.cecs445.R;
 import com.example.servermatch.cecs445.models.MenuItem;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.chip.ChipDrawable;
 import com.google.android.material.chip.ChipGroup;
 import com.google.android.material.chip.Chip;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageMetadata;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static android.app.Activity.RESULT_OK;
 
 public class AddMenuItemFragment extends Fragment {
 
@@ -33,10 +45,16 @@ public class AddMenuItemFragment extends Fragment {
     private TextInputLayout textInputItemDesc;
     private MaterialButton btnAddNewItem;
     private MaterialButton btnAddPhoto;
+    private ProgressBar progressBar;
     private static final String TAG = "AddMenuItemFragment";
     private ChipGroup chipGroup;
     private List<String> tags = new ArrayList<String>();
     private AddMenuItemViewModel addMenuItemViewModel;
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private StorageReference mStorageRef;
+    private boolean imageSelected;
+    private Uri imguri;
+    private StorageTask uploadTask;
 
     String [] filters = {
             "Vegan",
@@ -64,6 +82,9 @@ public class AddMenuItemFragment extends Fragment {
         textInputItemDesc = root.findViewById(R.id.text_input_item_desc);
         btnAddNewItem = root.findViewById((R.id.add_item_button));
         btnAddPhoto = root.findViewById((R.id.add_photo_button));
+        progressBar = root.findViewById((R.id.add_item_progress_bar));
+        mStorageRef = FirebaseStorage.getInstance().getReference("Images");
+        imageSelected = false;
 
         addMenuItemViewModel = new ViewModelProvider(this).get(AddMenuItemViewModel.class);
         addMenuItemViewModel.init();
@@ -97,7 +118,8 @@ public class AddMenuItemFragment extends Fragment {
         btnAddPhoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                addPhoto(v);
+                Log.d(TAG, "Add Photo Button Pressed");
+                choosePhoto();
             }
         });
 
@@ -124,6 +146,55 @@ public class AddMenuItemFragment extends Fragment {
         } else {
             textInputItemName.setError(null);
             return true;
+        }
+    }
+
+    private void choosePhoto() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+        imageSelected = true;
+        Log.d(TAG, "Image Selected from Gallery");
+    }
+
+    private String getExtension(Uri uri) {
+        ContentResolver cr = getContext().getContentResolver();
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(cr.getType(uri));
+    }
+
+    private void imageUploader() {
+        Log.d(TAG, "Image Upload IN PROGRESS");
+        progressBar.setVisibility(getView().VISIBLE);
+        btnAddNewItem.setEnabled(false);
+        StorageReference ref = mStorageRef.child(System.currentTimeMillis()+"."+getExtension(imguri));
+        ref.putFile(imguri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        // Get a URL to the uploaded content
+//                        Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                        Log.d(TAG, "Image upload SUCCESS");
+                        progressBar.setVisibility(getView().GONE);
+                        btnAddNewItem.setEnabled(true);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        // Handle unsuccessful uploads
+                        // ...
+                        Log.d(TAG, "Image upload FAILED");
+                    }
+                });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            imguri = data.getData();
         }
     }
 
@@ -176,11 +247,26 @@ public class AddMenuItemFragment extends Fragment {
         return false;
     }
 
+    private boolean validateImage() {
+        if(imageSelected == false) {
+            Log.d(TAG, "Add Item Button Pressed - No Image Selected");
+            Toast.makeText(getContext(), "No Image Selected", Toast.LENGTH_SHORT).show();
+            return false;
+        } else if(uploadTask != null && uploadTask.isInProgress()) {
+            Log.d(TAG, "Add Item Pressed while UPLOAD IN PROGRESS");
+            Toast.makeText(getContext(), "Upload in Progress", Toast.LENGTH_SHORT).show();
+            return false;
+        } else {
+            return true;
+        }
+    }
+
     //TODO: add new menu item here
     public void addItem(View v) {
-        if(!validateName() | !validateCost() | !validateDesc() | !validateFilters()) {
+        if(!validateName() | !validateCost() | !validateDesc() | !validateFilters() | !validateImage()) {
             return;
         }
+        imageUploader();
         String itemName = textInputItemName.getEditText().getText().toString();
         Double itemCost = Double.parseDouble(textInputItemCost.getEditText().getText().toString());
         String itemDescription = textInputItemDesc.getEditText().getText().toString();
@@ -189,17 +275,11 @@ public class AddMenuItemFragment extends Fragment {
         clearFields();
     }
 
-    //TODO: photo permissions
-    public void addPhoto(View v) {
-        Log.d(TAG, "Photo Button Pressed");
-        Toast.makeText(getContext(), "Add Photo", Toast.LENGTH_SHORT).show();
-    }
-
     private void clearFields(){
         textInputItemName.getEditText().setText("");
         textInputItemDesc.getEditText().setText("");
         textInputItemCost.getEditText().setText("");
         chipGroup.clearCheck();
-
+        imageSelected = false;
     }
 }
