09ca9f95416db77182084a1725e9e3bd2660302d
shadow88s <howardshowered@gmail.com>
2020-02-20 22:21:19 -0800

    -added first and last name to model

--- app/src/main/java/com/example/servermatch/cecs445/models/Customer.java
+++ app/src/main/java/com/example/servermatch/cecs445/models/Customer.java
@@ -6,6 +6,8 @@ package com.example.servermatch.cecs445.models;
  */
 
 public class Customer {
+    private String firstName;
+    private String lastName;
     private String email;
     private String phoneNum;
     private boolean receiptText;
@@ -13,6 +15,8 @@ public class Customer {
     private Integer visits;//number of Visits. going to somehow connect to each bill paid by customer.
 
     public Customer(String firstName, String lastName, String email, String phoneNum, boolean receiptText, boolean receiptEmail) {
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.email = email;
         this.phoneNum = phoneNum;
         this.receiptText = receiptText;
@@ -22,6 +26,22 @@ public class Customer {
     }
 
     public Customer(){}
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
@@ -61,64 +81,3 @@ public class Customer {
                 + '\'' + ", receiptText=" + receiptText + ", receiptEmail=" + receiptEmail + ']';
     }
 }
-
-//Parent -Child classes causes app to crash. Not sure why.
-//public class Customer extends Person {
-//    private String email;
-//    private String phoneNum;
-//    private boolean receiptText;
-//    private boolean receiptEmail;
-//    private Integer visits;//number of Visits. going to somehow connect to each bill paid by customer.
-//
-//    public Customer(String firstName, String lastName, String email, String phoneNum, boolean receiptText, boolean receiptEmail) {
-//        super(firstName, lastName);
-//        this.email = email;
-//        this.phoneNum = phoneNum;
-//        this.receiptText = receiptText;
-//        this.receiptEmail = receiptEmail;
-//        this.visits = 0;
-//
-//    }
-//
-//    public Customer(String firstName, String lastName){
-//        super(firstName, lastName);
-//    }
-//
-//    public String getEmail() { return email; }
-//    public void setEmail(String email) { this.email = email; }
-//
-//    public String getPhoneNum() { return phoneNum; }
-//    public void setPhone(String phoneNum) { this.phoneNum = phoneNum; }
-//
-//    public boolean getRecepitText() { return receiptText; }
-//    public void setReceiptText(boolean receiptText) { this.receiptText = receiptText; }
-//
-//    public boolean getRecepitEmail() { return receiptEmail; }
-//    public void setReceiptEmail(boolean receiptEmail) { this.receiptEmail = receiptEmail; }
-//
-//    public void setPhoneNum(String phoneNum) {
-//        this.phoneNum = phoneNum;
-//    }
-//
-//    public boolean isReceiptText() {
-//        return receiptText;
-//    }
-//
-//    public boolean isReceiptEmail() {
-//        return receiptEmail;
-//    }
-//
-//    public Integer getVisits() {
-//        return visits;
-//    }
-//
-//    public void setVisits(Integer visits) {
-//        this.visits = visits;
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return "Customer[super=" + super.toString() + "email='" + email + '\'' + ", phoneNum='" + phoneNum
-//                + '\'' + ", receiptText=" + receiptText + ", receiptEmail=" + receiptEmail + ']';
-//    }
-//}
