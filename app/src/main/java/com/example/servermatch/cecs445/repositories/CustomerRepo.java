3f8c84e3bde2f0230fb9438f40f86c5cfa7c8ff4
shadow88s <howardshowered@gmail.com>
2020-02-22 10:16:42 -0800

    -added checkOutCustomer to CustomerRepo

--- app/src/main/java/com/example/servermatch/cecs445/repositories/CustomerRepo.java
+++ app/src/main/java/com/example/servermatch/cecs445/repositories/CustomerRepo.java
@@ -1,31 +1,44 @@
 package com.example.servermatch.cecs445.repositories;
 
 
+import android.content.Context;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.MutableLiveData;
 
+import com.example.servermatch.cecs445.models.Bill;
 import com.example.servermatch.cecs445.models.Customer;
+import com.example.servermatch.cecs445.models.MenuItem;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author Howard
  */
 public class CustomerRepo {
 
+    private static final String TAG = "CustomerRepo";
     private static CustomerRepo instance;
     private ArrayList<Customer> dataSet = new ArrayList<>();
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
-    private static final String TAG = "CustomerRepo";
+    private  final CollectionReference customerRef =
+            db.collection("Customer");
+    private  final CollectionReference billRef =
+            db.collection("Bill");
 
     public static CustomerRepo getInstance(){
         if(instance == null){
@@ -43,7 +56,7 @@ public class CustomerRepo {
     }
 
     private void loadCustomers(){
-        db.collection("Customer").get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+        customerRef.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
             @Override
             public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
 
@@ -67,12 +80,44 @@ public class CustomerRepo {
     }
 
     public void addCustomer(Customer newCustomer){
-        db.collection("Customer").add(newCustomer).addOnSuccessListener
-                (new OnSuccessListener<DocumentReference>() {
+//        customerRef.add(newCustomer).addOnSuccessListener
+//                (new OnSuccessListener<DocumentReference>() {
+//                    @Override
+//            public void onSuccess(DocumentReference documentReference) {
+//                //Where Toast would go for success
+//                        //still needs to figure out how to Toast from Repo Class.
+//            }
+//        }).addOnFailureListener(new OnFailureListener() {
+//            @Override
+//            public void onFailure(@NonNull Exception e) {
+//                Log.e(TAG, "onFailure: " + e.toString());
+//            }
+//        });
+
+        customerRef.document(newCustomer.getEmail()).set(newCustomer)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
-            public void onSuccess(DocumentReference documentReference) {
-                //Where Toast would go for success
-                        //still needs to figure out how to Toast from Repo Class.
+                    public void onSuccess(Void aVoid) {
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+            }
+        });
+
+        HashMap<String, Integer> itemMap = new HashMap<>();
+        MenuItemRepo mRepo = MenuItemRepo.getInstance();
+        List<MenuItem> menuItemList = mRepo.getMenuItems().getValue();
+        for(MenuItem menuItem: menuItemList){
+            itemMap.put(menuItem.getDocumentId(), 0);
+        }
+        billRef.document(newCustomer.getEmail()).set(itemMap)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+
             }
         }).addOnFailureListener(new OnFailureListener() {
             @Override
@@ -82,5 +127,28 @@ public class CustomerRepo {
         });
     }
 
+    public void checkOutCustomer(Context context, Customer customer,
+                                 List<MenuItem> billItems) {
 
+        //increments visit
+        String email  = customer.getEmail();
+        customerRef.document(email)
+                .set(customer).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Toast.makeText(context, "Visit updated!", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.e(TAG, "onFailure: " + e.toString());
+            }
+        });
+
+        for(MenuItem item: billItems){
+            //need to check if menuItem has been ordered before or not.
+            billRef.document(email).update(item.getDocumentId(), FieldValue.increment(item.getmIntQuantity()));
+        }
+
+    }
 }
