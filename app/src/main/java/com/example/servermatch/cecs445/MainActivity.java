e37ae3b406bb15cbf8d93668ce631886681499e8
Juan Carlos Pasillas <37219813+JCPasillas@users.noreply.github.com>
2020-02-25 08:59:35 -0800

    filter menu fragment created

--- app/src/main/java/com/example/servermatch/cecs445/MainActivity.java
+++ app/src/main/java/com/example/servermatch/cecs445/MainActivity.java
@@ -2,11 +2,16 @@ package com.example.servermatch.cecs445;
 
 import android.os.Bundle;
 
+import com.example.servermatch.cecs445.ui.menu.FilterMenuFragment;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -20,12 +25,14 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.view.Menu;
+import android.widget.Filter;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
+    private FrameLayout fragmentContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +53,10 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        //Filter Menu
+        fragmentContainer = (FrameLayout) findViewById(R.id.fragment_container);
+
     }
 
     @Override
@@ -56,9 +67,32 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.action_filter:
+                Toast.makeText(this, "Menu Filter", Toast.LENGTH_SHORT).show();
+                //openFragmentMenuFilter();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    public void openFragmentMenuFilter() {
+        FilterMenuFragment filterMenuFragment = FilterMenuFragment.newInstance();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.popBackStack();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        //transaction.setCustomAnimations(R.anim.slide_in_right, R.anim.slide_out_right, R.anim.slide_in_right, R.anim.slide_out_right);
+        //transaction.addToBackStack(null);
+       //transaction.add(R.id.fragment_container, filterMenuFragment, "FILTER_MENU_FRAGMENT").commit();
+        transaction.replace(R.id.fragment_container, filterMenuFragment, "FILTER_MENU_FRAGMENT").commit();
     }
 }
