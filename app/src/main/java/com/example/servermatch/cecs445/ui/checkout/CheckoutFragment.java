70b5793775e01e723155b329d41e20dea350943e
shadow88s <howardshowered@gmail.com>
2020-02-24 21:05:01 -0800

    validates email format.

--- app/src/main/java/com/example/servermatch/cecs445/ui/checkout/CheckoutFragment.java
+++ app/src/main/java/com/example/servermatch/cecs445/ui/checkout/CheckoutFragment.java
@@ -8,16 +8,17 @@ package com.example.servermatch.cecs445.ui.checkout;
  */
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -25,8 +26,9 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.servermatch.cecs445.R;
 import com.example.servermatch.cecs445.Utils.CheckoutRecyclerAdapter;
 import com.example.servermatch.cecs445.models.Bill;
-import com.example.servermatch.cecs445.models.Customer;
 import com.example.servermatch.cecs445.models.MenuItem;
+import com.example.servermatch.cecs445.ui.menu.MenuFragment;
+import com.google.android.material.textfield.TextInputLayout;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -41,7 +43,7 @@ public class CheckoutFragment extends Fragment {
     private Bill bill;
     private RecyclerView recyclerView;
     private TextView totalCost;
-    private EditText mEmail;
+    private TextInputLayout mEmail;
     private Button mCheckoutButton;
 
 
@@ -65,18 +67,28 @@ public class CheckoutFragment extends Fragment {
 
     public void checkoutButtonListener(){
         mCheckoutButton.setOnClickListener(v -> {
-            //get checkout time
-            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm:ss");
-            LocalDateTime now = LocalDateTime.now();
-            String checkoutTime= dtf.format(now);
+            boolean validEmail = validateEmail();
+            if(validEmail) {
+                //get checkout time
+                DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm:ss");
+                LocalDateTime now = LocalDateTime.now();
+                String checkoutTime = dtf.format(now);
+                String emailString = mEmail.getEditText().getText().toString().trim().toLowerCase();
+                bill.setCustomerID(emailString);
+                checkOutViewModel.checkOutCustomer(getContext(), bill, checkoutTime);
+                Log.d(TAG, mEmail.getEditText().toString());
+                Log.d(TAG, bill.toString());
 
-            String emailString = mEmail.getText().toString().toLowerCase();
-            bill.setCustomerID(emailString);
-            checkOutViewModel.checkOutCustomer(getContext(), bill, checkoutTime);
+                MenuFragment menuFragment = new MenuFragment();
+                FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
+                transaction.setCustomAnimations(R.anim.slide_in_right,R.anim.slide_out_right,R.anim.slide_in_right,R.anim.slide_out_right);
+                transaction.replace(R.id.nav_host_fragment, menuFragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
+                //goes back a MenuFragment
 
+            }
 
-            Log.d(TAG, mEmail.getText().toString());
-            Log.d(TAG, bill.toString());
         });
     }
 
@@ -113,6 +125,20 @@ public class CheckoutFragment extends Fragment {
 
             bill = new Bill((double)bundle.get("billTotal"),menuItems);
             totalCost.setText("$" + String.format("%.2f",bundle.get("billTotal")));
+        }
+    }
+
+    private boolean validateEmail() {
+        String emailInput = mEmail.getEditText().getText().toString().trim();
+        if(emailInput.isEmpty()) {
+            mEmail.setError("Field can't be empty");
+            return false;
+        } else if(!Patterns.EMAIL_ADDRESS.matcher(emailInput).matches()) {
+            mEmail.setError("Invalid email address");
+            return false;
+        } else {
+            mEmail.setError(null);
+            return true;
         }
     }
 }
