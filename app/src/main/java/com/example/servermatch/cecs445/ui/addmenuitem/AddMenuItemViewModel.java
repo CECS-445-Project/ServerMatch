b1027a0084d2c4be0683c0ee6a53ab71810d938d
shadow88s <howardshowered@gmail.com>
2020-02-26 07:02:20 -0800

    Revert "- fixed duplicate MenuItems bug when adding new MenuItems - cleaned up code and removed deprecated comments"
    This reverts commit a72facf6

--- app/src/main/java/com/example/servermatch/cecs445/ui/addmenuitem/AddMenuItemViewModel.java
+++ app/src/main/java/com/example/servermatch/cecs445/ui/addmenuitem/AddMenuItemViewModel.java
@@ -5,7 +5,10 @@ import android.content.Context;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+
+import com.example.servermatch.cecs445.models.Customer;
 import com.example.servermatch.cecs445.models.MenuItem;
+import com.example.servermatch.cecs445.repositories.CustomerRepo;
 import com.example.servermatch.cecs445.repositories.MenuItemRepo;
 
 import java.util.List;
@@ -27,8 +30,8 @@ public class AddMenuItemViewModel extends ViewModel {
 
     public void addMenuItem(MenuItem newItem, Context context){
         mRepo.addMenuItem(newItem, context);
-        //clears offline list to prevent duplicate data from firestore listener
-        List<MenuItem> currentItems = null;
+        List<MenuItem> currentItems = mMenuItem.getValue();
+        currentItems.add(newItem);
         mMenuItem.postValue(currentItems);
     }
     public LiveData<List<MenuItem>> getMenuItems() { return mMenuItem; }
