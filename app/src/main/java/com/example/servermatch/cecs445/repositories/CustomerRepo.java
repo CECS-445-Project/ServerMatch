a213c35b5b34778c8cd79aa28af86d04d3e383f4
shadow88s <howardshowered@gmail.com>
2020-02-25 22:54:49 -0800

    - added check for registered customers - added guest firestore collection to database - cleaned up code and removed comment reminders

--- app/src/main/java/com/example/servermatch/cecs445/repositories/CustomerRepo.java
+++ app/src/main/java/com/example/servermatch/cecs445/repositories/CustomerRepo.java
@@ -1,13 +1,11 @@
 package com.example.servermatch.cecs445.repositories;
 
-
 import android.content.Context;
 import android.util.Log;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.lifecycle.MutableLiveData;
-
 import com.example.servermatch.cecs445.models.Bill;
 import com.example.servermatch.cecs445.models.Customer;
 import com.example.servermatch.cecs445.models.MenuItem;
@@ -15,14 +13,15 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QuerySnapshot;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 
 /**
  * @author Howard Chen
@@ -55,27 +54,8 @@ public class CustomerRepo {
     }
 
     private void loadCustomers(){
-        customerRef.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
-            @Override
-            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-
-                if(!queryDocumentSnapshots.isEmpty()){
-                    List<DocumentSnapshot> list = queryDocumentSnapshots.getDocuments();
-
-                    for(DocumentSnapshot documentSnapshot : list){
-                        dataSet.add(documentSnapshot.toObject(Customer.class));
-                    }
-                }
-
-                Log.e(TAG, "onSuccess: added" );
-
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Log.e(TAG, "onFailure: " + e.toString());
-            }
-        });
+        //listens for updates on Collection in realtime
+        collectionListener("Customer");
     }
 
     public void addCustomer(Customer newCustomer){
@@ -104,14 +84,13 @@ public class CustomerRepo {
 
     }
 
-    public void incrementVisit(Context context, String email){
+    private void incrementVisit(Context context, String email){
 
         customerRef.document(email)
                 .update("visits", FieldValue.increment(1))
                 .addOnSuccessListener(new OnSuccessListener<Void>() {
             @Override
             public void onSuccess(Void aVoid) {
-                Toast.makeText(context, "Visits incremented!", Toast.LENGTH_SHORT).show();
                 incrementedVisit = true;
             }
         }).addOnFailureListener(new OnFailureListener() {
@@ -124,28 +103,83 @@ public class CustomerRepo {
 
     }
 
-    public void storeBill(Context context, String email, Bill bill, String checkoutTime){
-        Map<String, Object> billInformation =  new HashMap<>();
+    private void storeBill(Context context, String email, Bill bill, String checkoutTime){
+
+        String collectionName;
+        String toastMsg;
+        Map<String, Object> billInformation = new HashMap<>();
         billInformation.put("TransactionDateTime:", checkoutTime);
         List<MenuItem> billItems = bill.getBillItems();
-        for(MenuItem menuItem : billItems){
+        for (MenuItem menuItem : billItems) {
             String menuItemName = menuItem.getItemName();
-            Integer menuItemQty =  menuItem.getmIntQuantity();
+            Integer menuItemQty = menuItem.getmIntQuantity();
             billInformation.put(menuItemName, menuItemQty);
         }
 
-        customerRef.document(email).collection("Bills").document(checkoutTime)
+        if(checkEmail(email)) {
+            collectionName = "Customer";
+            toastMsg = "as a register customer";
+        }else{
+            collectionName = "Guest";
+            toastMsg = "as a guest";
+        }
+
+        db.collection(collectionName).document(email).collection("Bills").document(checkoutTime)
                 .set(billInformation).addOnSuccessListener(new OnSuccessListener<Void>() {
             @Override
             public void onSuccess(Void aVoid) {
-                Toast.makeText(context, "Bill Stored @" + checkoutTime, Toast.LENGTH_SHORT).show();
+                Toast.makeText(context, "Checkout Completed for " + email + "\n@" + checkoutTime + toastMsg, Toast.LENGTH_SHORT).show();
             }
         }).addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(@NonNull Exception e) {
-                Log.e(TAG, "onFailure: " + e.toString() );
+                Log.e(TAG, "onFailure: " + e.toString());
             }
         });
-
     }
+
+    private boolean checkEmail(String email){
+        loadCustomers(); //loads dataset with new query
+        for(Customer customer : dataSet){
+            if(customer.getEmail().equals(email)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void collectionListener(String collection){
+
+        db.collection(collection).addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
+                if(e != null){
+                    Log.w(TAG,"Listen Failed", e );
+                    return;
+                }
+
+                db.collection(collection).get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+
+                        if(!queryDocumentSnapshots.isEmpty()){
+                            List<DocumentSnapshot> list = queryDocumentSnapshots.getDocuments();
+                            for(DocumentSnapshot documentSnapshot : list){
+                                dataSet.add(documentSnapshot.toObject(Customer.class));
+                            }
+                        }
+
+                        Log.e(TAG, "onSuccess: added" );
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.e(TAG, "onFailure: " + e.toString());
+                    }
+                });
+            }
+        });
+    }
+
 }
