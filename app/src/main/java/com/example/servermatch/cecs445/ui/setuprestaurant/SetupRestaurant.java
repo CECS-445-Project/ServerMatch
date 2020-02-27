70cee0c91aca30f308e2ef8f1bd0afc6a84e1d53
Juan Carlos Pasillas <37219813+JCPasillas@users.noreply.github.com>
2020-02-27 08:28:45 -0800

    created a SetupRestaurant Activity to boot first using manifest created a SetUpRestaurant Fragment to intake restaurant information changed the word 'add' to 'setup' for restaurant files removed addRestaurant from sidebar navigator added the textviews to the mainActivity for restaurant navigation label added strings for restaurant label added image for setup activity

---
+++ app/src/main/java/com/example/servermatch/cecs445/ui/setuprestaurant/SetupRestaurant.java
@@ -0,0 +1,41 @@
+package com.example.servermatch.cecs445.ui.setuprestaurant;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.servermatch.cecs445.R;
+
+import java.util.Set;
+
+public class SetupRestaurant extends AppCompatActivity {
+
+    Button btnSetupRestaurant;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setup_restaurant);
+
+        btnSetupRestaurant = findViewById(R.id.setup_restaurant_button);
+        btnSetupRestaurant.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //startActivity(new Intent(SetupRestaurant.this, MainActivity.class));
+                openRestaurantSetup();
+            }
+        });
+    }
+
+    public void openRestaurantSetup() {
+        SetupRestaurantFragment restaurantFragment = SetupRestaurantFragment.newInstance();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.addToBackStack(null);
+        transaction.add(R.id.setup_restaurant_container, restaurantFragment, "SETUP RESTAURANT FRAGMENT").commit();
+    }
+}
---
+++ app/src/main/java/com/example/servermatch/cecs445/ui/setuprestaurant/SetupRestaurantFragment.java
@@ -0,0 +1,128 @@
+package com.example.servermatch.cecs445.ui.setuprestaurant;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+
+import com.example.servermatch.cecs445.MainActivity;
+import com.example.servermatch.cecs445.R;
+import com.example.servermatch.cecs445.models.Restaurant;
+import com.google.android.material.textfield.TextInputLayout;
+
+public class SetupRestaurantFragment extends Fragment {
+
+    public static final String EXTRA_RESTAURANT_NAME = "com.example.servermatch.cecs445.ui.setuprestaurant.EXTRA_RESTAURANT_NAME";
+    public static final String EXTRA_RESTAURANT_EMAIL = "com.example.servermatch.cecs445.ui.setuprestaurant.EXTRA_RESTAURANT_EMAIL";
+    public static final String EXTRA_RESTAURANT_PHONE = "com.example.servermatch.cecs445.ui.setuprestaurant.EXTRA_RESTAURANT_PHONE";
+
+    private SetupRestaurantViewModel setupRestaurantViewModel;
+    private TextInputLayout setupRestaurantName;
+    private TextInputLayout setupRestaurantEmail;
+    private TextInputLayout setupRestaurantPhone;
+    private Button btnSetupRestaurant;
+
+    public SetupRestaurantFragment() {
+        //blank constructor
+    }
+
+    public static SetupRestaurantFragment newInstance() {
+        SetupRestaurantFragment fragment = new SetupRestaurantFragment();
+        return fragment;
+    }
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        setupRestaurantViewModel = new ViewModelProvider(this).get(SetupRestaurantViewModel.class);
+
+        View root = inflater.inflate(R.layout.fragment_setup_restaurant, container, false);
+
+        setupRestaurantName = root.findViewById(R.id.setup_restaurant_name);
+        setupRestaurantEmail = root.findViewById(R.id.setup_restaurant_email);
+        setupRestaurantPhone = root.findViewById(R.id.setup_restaurant_phone);
+        btnSetupRestaurant = root.findViewById(R.id.setup_restaurant_submit);
+
+        btnSetupRestaurant.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String restaurantName = setupRestaurantName.getEditText().getText().toString();
+                String restaurantEmail = setupRestaurantEmail.getEditText().getText().toString().toLowerCase();
+                String restaurantPhone = setupRestaurantPhone.getEditText().getText().toString();
+
+                Intent intent = new Intent(getActivity(), MainActivity.class);
+                intent.putExtra(EXTRA_RESTAURANT_NAME, restaurantName);
+                intent.putExtra(EXTRA_RESTAURANT_EMAIL, restaurantEmail);
+                intent.putExtra(EXTRA_RESTAURANT_PHONE, restaurantPhone);
+                startActivity(intent);
+
+                //TODO: Implement the validation using database
+                //validateInput(restaurantName, restaurantEmail, restaurantPhone);
+                //Restaurant newRestaurant = new Restaurant(restaurantName, restaurantEmail, restaurantPhone);
+                //setupRestaurantViewModel.setupRestaurant(newRestaurant);
+            }
+        });
+
+        return root;
+    }
+
+    private boolean validateName(String name) {
+        if(name.isEmpty()) {
+            setupRestaurantName.setError("Field can't be empty");
+            return false;
+        } else {
+            setupRestaurantName.setError(null);
+            return true;
+        }
+    }
+
+    private boolean validateEmail(String email) {
+        if(email.isEmpty()) {
+            setupRestaurantEmail.setError("Field can't be empty");
+            return false;
+        } else if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            setupRestaurantEmail.setError("Invalid email setupress");
+            return false;
+        } else {
+            setupRestaurantEmail.setError(null);
+            return true;
+        }
+    }
+
+    private boolean validatePhone(String phone) {
+        String regexPhone = "^[0-9]+$";
+        if (phone.isEmpty()) {
+            setupRestaurantPhone.setError("Field can't be empty");
+            return false;
+        } else if (phone.length() < 10 || !phone.matches(regexPhone)) {
+            setupRestaurantPhone.setError("Invalid phone number");
+            return false;
+        } else {
+            setupRestaurantPhone.setError(null);
+            return true;
+        }
+    }
+
+        public void validateInput(String name, String email, String phone) {
+            if(!validateName(name) | !validateEmail(email) | !validatePhone(phone)) {
+                return;
+            }
+
+            Restaurant r1 = new Restaurant(name, phone, email);
+            Log.d("setup_restaurant", r1.toString());
+            Toast.makeText(getContext(), "Restaurant Created", Toast.LENGTH_SHORT).show();
+        }
+
+}
