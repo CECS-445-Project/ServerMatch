2cc847e42ae40e397e5d64229bef4829a0654042
Andrew-Delgado <andrewdelgado017@gmail.com>
2020-03-02 12:30:23 -0800

    fixed a sytle

--- app/src/main/java/com/example/servermatch/cecs445/ui/filters/FiltersFragment.java
+++ app/src/main/java/com/example/servermatch/cecs445/ui/filters/FiltersFragment.java
@@ -8,18 +8,18 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import com.example.servermatch.cecs445.R;
 import com.google.android.material.chip.Chip;
 import com.google.android.material.chip.ChipDrawable;
 import com.google.android.material.chip.ChipGroup;
 
+import java.util.List;
+
 public class FiltersFragment extends Fragment {
 
     private View view;
     private ChipGroup mChipGroup;
+    private List<String> filters;
 
     @Nullable
     @Override
@@ -28,31 +28,15 @@ public class FiltersFragment extends Fragment {
         view = inflater.inflate(R.layout.fragment_filters, container,false);
         mChipGroup = view.findViewById(R.id.chip_group_filters);
 
+        Chip c = (Chip) mChipGroup.getCheckedChipId();
+
         initChipGroup();
         return view;
     }
 
     private void initChipGroup(){
 
-        String [] filter = {
-                "Vegan",
-                "Vegetarian",
-                "Dairy Free",
-                "Gluten Free",
-                "Soy Free",
-                "Low-Carb",
-                "High Protein",
-                "Spicy",
-                "Contains Eggs",
-                "Contains Dairy",
-                "Contains Nuts",
-                "Contains Fish",
-                "Contains Beef",
-                "Contains Poultry",
-                "Contains Pork"
-        };
-
-        for(String s:filter){
+        for(String s: filters){
             Chip chip = new Chip(getContext());
             ChipDrawable chipDrawable = ChipDrawable.createFromAttributes(getContext(), null, 0, R.style.CustomChipChoice);
             chip.setChipDrawable(chipDrawable);
@@ -62,3 +46,21 @@ public class FiltersFragment extends Fragment {
         }
     }
 }
+
+//    private String [] filter = {
+//            "Vegan",
+//            "Vegetarian",
+//            "Dairy Free",
+//            "Gluten Free",
+//            "Soy Free",
+//            "Low-Carb",
+//            "High Protein",
+//            "Spicy",
+//            "Contains Eggs",
+//            "Contains Dairy",
+//            "Contains Nuts",
+//            "Contains Fish",
+//            "Contains Beef",
+//            "Contains Poultry",
+//            "Contains Pork"
+//    };
